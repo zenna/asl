279f4a37838b92544cefb645d1699814891403cd
Zenna Tavares <zennatavares@gmail.com>
2017-10-04 20:21:04 -0400

    forward pass completes

--- examples/stack.py
+++ examples/stack.py
@@ -1,153 +1,153 @@
-from mnist import *
-from pdt.train_tf import *
-from wacacore.util.misc import *
-from wacacore.util.io import mk_dir, handle_args, gen_sfx_key
-from wacacore.util.generators import infinite_samples, infinite_batches
-from pdt.types import *
-from common import default_benchmark_options
-from tensortemplates.util.io import add_additional_options
+"Stack Data Structure trained from a reference implementation"
+import sys
+import numpy as np
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import itertools
+
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+class PushNet(nn.Module):
+    def __init__(self, name, stack_channels=1, img_channels=3):
+        super(PushNet, self).__init__()
+        in_channels = stack_channels + img_channels
+        out_channels = stack_channels
+        self.name = name
+        self.conv1 = nn.Conv2d(in_channels, 1, 1)
+        self.conv2 = nn.Conv2d(1, stack_channels, 1)
+
+    def forward(self, *x):
+        if len(x) > 1:
+            x = torch.cat(x, dim=1)
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        return (x,)
 
 
-def stack_adt(train_data,
-              options,
-              stack_shape=(28, 28, 1),
-              push_args={},
-              pop_args={},
-              empty_stack_args={},
-              item_shape=(28, 28, 1),
-              batch_size=512,
-              nitems=3):
+class PopNet(nn.Module):
+    def __init__(self, name, stack_channels=1, img_channels=3):
+        super(PopNet, self).__init__()
+        self.stack_channels = stack_channels
+        self.img_channels = img_channels
+        out_channels = stack_channels + img_channels
+        self.name = name
+        self.conv1 = nn.Conv2d(stack_channels, 1, 1)
+        self.conv2 = nn.Conv2d(1, out_channels, 1)
+
+    def forward(self, *x):
+        channel_dim = 1
+        x, = x
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        (img, stack) = x.split(self.img_channels, channel_dim)
+        return (stack, img)
+
+
+def net_stack():
     """Construct a stack abstract data type"""
-    # Types - a Stack of Item
-    Stack = Type(stack_shape, 'Stack')
-    Item = Type(item_shape, 'Item')
-
-    # Interface
-
-    # Push an Item onto a Stack to create a new stack
-    push = Interface([Stack, Item], [Stack], 'push', **push_args)
-    # Pop an Item from a stack, returning a new stack and the item
-    pop = Interface([Stack], [Stack, Item], 'pop', **pop_args)
-    interfaces = [push, pop]
-
-    # train_outs
-    train_outs = []
-    gen_to_inputs = identity
-
-    # Consts
-    # The empty stack is the stack with no items
-    empty_stack = Const(Stack, 'empty_stack', batch_size, **empty_stack_args)
-    consts = [empty_stack]
-
-    # Vars
-    # stack1 = ForAllVar(Stack)
-    items = [ForAllVar(Item, str(i)) for i in range(nitems)]
-    forallvars = items
-
-    generators = [infinite_batches(train_data, batch_size, shuffle=True)
-                  for i in range(nitems)]
-
-    # Axioms
-    axioms = []
-    stack = empty_stack.batch_input_var
-    stacks = [stack]
-    for i in range(nitems):
-        (stack,) = push(stack, items[i].input_var)
-        stacks.append(stack)
-        pop_stack = stack
-        for j in range(i, -1, -1):
-            (pop_stack, pop_item) = pop(pop_stack)
-            axiom = Axiom((pop_item,), (items[j].input_var,), 'item-eq%s-%s' %(i, j))
-            axioms.append(axiom)
-
-            # Stack equivalence
-            # axiom = Axiom((pop_stack,), (stacks[j],), 'stack-eq%s-%s' %(i, j))
-            # axioms.append(axiom)
-    # train_fn, call_fns = compile_fns(interfaces, consts, forallvars, axioms,
-                                    #  train_outs, options)
-    train_fn = None
-    call_fns = None
-    stack_adt = AbstractDataType(interfaces, consts, forallvars, axioms,
-                                 name='stack')
-    stack_pdt = ProbDataType(stack_adt, train_fn, call_fns,
-                             generators, gen_to_inputs, train_outs)
-    return stack_adt, stack_pdt
+    push = PushNet("push")
+    pop = PopNet("pop")
+    empty = Variable(torch.rand(4, 1, 32, 32), requires_grad=True)
+    constants = {'empty': empty}
+    interface = {'push': push, 'pop': pop}
+    # FIXME!
+    # empty = empty.expand(4, 1, 32, 32)
+    return {'constants': constants, 'interface': interface}
 
 
-def mse(a, b):
-    return np.mean(np.square(a - b))
+def list_push(stack, element):
+    stack = stack.copy()
+    stack.append(element)
+    return (stack, )
 
 
-# Check for loss from one example of pushing and popping
-def stack_unstack(nitems, push, pop, empty, items):
+def list_pop(stack):
+    stack = stack.copy()
+    item = stack.pop()
+    return (stack, item)
+
+
+def py_stack():
+    empty_stack = []
+    return {'constants': {'empty': empty_stack},
+            'interface': {'push': list_push, 'pop': list_pop}}
+
+
+def stack_trace(model, items):
+    """Example stack trace"""
+    items = [data[0] for data in list(itertools.islice(items, 3))]
+    push = model['interface']['push']
+    pop = model['interface']['pop']
+    empty = model['constants']['empty']
+    observes = []
     stack = empty
-    stacks = [stack]
-    losses = []
-    for i in range(nitems):
-        (stack,) = push(stack, items[i])
-        stacks.append(stack)
-        pop_stack = stack
-        for j in range(i, -1, -1):
-            (pop_stack, pop_item) = pop(pop_stack)
-            loss = mse(pop_item, items[j])
-            losses.append(loss)
-    return np.sum(losses), losses
+    (stack,) = push(stack, items[0])
+    (stack,) = push(stack, items[1])
+    (stack,) = push(stack, items[2])
+    (pop_stack, pop_item) = pop(stack)
+    observes.append(pop_item)
+    (pop_stack, pop_item) = pop(pop_stack)
+    observes.append(pop_item)
+    return observes
 
 
-def mnistshow(x):
-    plt.imshow(x.reshape(28, 28))
+def model_params(model):
+    iparams = []
+    for (name, net) in model['interface'].items():
+        for params in net.parameters():
+            iparams.append(params)
+    cparams = [var for (name, var) in model['constants'].items()]
+    return iparams + cparams
 
 
-def internal_plot(images, push, pop, empty):
-    stack = empty
-    for i in range(len(images)):
-        mnistshow(stack)
-        plt.figure()
-        (stack,) = push(stack, images[i])
-    mnistshow(stack)
+def train(trainloader, reference, model):
+    criterion = nn.CrossEntropyLoss()
+    criterion = nn.MSELoss()
+    optimizer = optim.SGD(model_params(model), lr=0.001, momentum=0.9)
 
+    for epoch in range(2):  # loop over the dataset multiple times
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs
+            observes = stack_trace(model, trainloader)
+            refobserves = stack_trace(reference, trainloader)
 
-def default_stack_options():
-    """Default options for scalar field"""
-    return {'field_shape': (eval, "(16, 16)"),
-            'name': (str, 'Stack'),
-            'nitems': (int, 3)}
+            total_loss = 0.0
+            for i in range(len(observes)):
+                loss = criterion(observes[i], refobserves[i])
+                total_loss += loss
+            # zero the parameter gradients
+            optimizer.zero_grad()
 
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = total_loss
+            loss.backward()
+            optimizer.step()
 
-def combine_options():
-    """Get options by combining default and command line"""
-    cust_options = {}
-    argv = sys.argv[1:]
+            # print statistics
+            running_loss += loss.data[0]
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
 
-    # add default options like num_iterations
-    cust_options.update(default_benchmark_options())
-
-    # add render specific options like width/height
-    cust_options.update(default_stack_options())
-
-    # Update with options passed in through command line
-    cust_options.update(add_additional_options(argv))
-
-    options = handle_args(argv, cust_options)
-    return options
+    print('Finished Training')
 
 
 def main(argv):
-    global adt, pdt, sess, X_train, sfx
-    options = combine_options()
-    mnist_data = load_dataset()
-    X_train = mnist_data[0].reshape(-1, 28, 28, 1)
-    empty_stack_args = {'initializer': tf.random_uniform_initializer}
-    adt, pdt = stack_adt(X_train,
-                         options,
-                         push_args=options,
-                         nitems=options['nitems'],
-                         pop_args=options,
-                         empty_stack_args=empty_stack_args,
-                         batch_size=options['batch_size'])
-
-    options['dirname'] = gen_sfx_key(('adt', 'nitems'), options)
-    sess = train(adt, pdt, options)
+    transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=2)
+    train(trainloader, py_stack(), net_stack())
 
 
 if __name__ == "__main__":
