99c59204b333e85fbc561320498462857468d026
Zenna Tavares <zennatavares@gmail.com>
2017-10-12 11:09:27 -0400

    move stack tests to tests

--- asl/structs/nstack.py
+++ asl/structs/nstack.py
@@ -1,15 +1,8 @@
 "Stack Data Structure trained from a reference implementation"
-import itertools
-
-from asl.type import Type, Function
-from asl.train import trainloss, log, log_append
+from asl.type import Function
 from asl.modules import VarConvNet, ConstantNet, ModuleDict
-from asl.callbacks import tb_loss, every_n
-from asl.util import draw, trainloader, as_img, iterget
 
-import torch
-from torch.autograd import Variable
-from torch import optim, nn
+from torch import nn
 
 
 class Push(Function):
@@ -60,39 +53,6 @@ def list_pop(stack):
   return (stack, item)
 
 
-def stack_trace(items, push, pop, empty):
-    """Example stack trace"""
-    log_append("empty", empty)
-
-    observes = []
-    stack = empty
-    # print("mean", items[0].mean())
-    (stack,) = push(stack, items[0])
-    (stack,) = push(stack, items[1])
-    # (stack,) = push(stack, items[2])
-    (pop_stack, pop_item) = pop(stack)
-    observes.append(pop_item)
-    (pop_stack, pop_item) = pop(pop_stack)
-    observes.append(pop_item)
-    log_append("observes", observes)
-    return observes
-
-
-def plot_observes(i, log, writer, **kwargs):
-  "Show the empty set in tensorboardX"
-  refobserves = log['observes'][0]
-  nobserves = log['observes'][1]
-  for j in range(len(refobserves)):
-    writer.add_image('compare{}/ref'.format(j), refobserves[j][0], i)
-    writer.add_image('compare{}/neural'.format(j), nobserves[j][0], i)
-
-
-def plot_empty(i, log, writer, **kwargs):
-  "Show the empty set in tensorboardX"
-  img = log['empty'][0][0]
-  writer.add_image('EmptySet', img, i)
-
-
 def neural_stack(element_type, stack_type):
   push_img = PushNet(stack_type, element_type)
   pop_img = PopNet(stack_type, element_type)
@@ -107,47 +67,3 @@ def neural_stack(element_type, stack_type):
 
 def ref_stack(element_type, stack_type):
   return {"push": list_push, "pop": list_pop, "empty": []}
-
-
-def observe_loss(criterion, obs, refobs, state=None):
-  "MSE between observations from reference and training stack"
-  total_loss = 0.0
-  losses = [criterion(obs[i], refobs[i]) for i in range(len(obs))]
-  print([loss[0].data[0] for loss in losses])
-  total_loss = sum(losses)
-  return total_loss
-
-
-def main():
-  batch_size = 128
-  tl = trainloader(batch_size)
-  dataiter = iter(tl)
-  refiter = iter(tl)
-  matrix_stack = Type("Stack", (1, 28, 28), dtype="float32")
-  mnist_type = Type("mnist_type", (1, 28, 28), dtype="float32")
-  nstack = neural_stack(mnist_type, matrix_stack)
-  refstack = ref_stack(mnist_type, matrix_stack)
-
-  criterion = nn.MSELoss()
-  optimizer = optim.Adam(nstack.parameters(), lr=0.0001)
-
-  def loss_gen():
-    try:
-      items = iterget(dataiter, 3)
-      ref_items = iterget(refiter, 3)
-    except StopIteration:
-      print("End of Epoch")
-      items_iter = iter(tl)
-      ref_items_iter = iter(tl)
-      items = iterget(items_iter, 3)
-      ref_items = iterget(ref_items_iter, 3)
-
-
-    observes = stack_trace(items, **nstack)
-    refobserves = stack_trace(ref_items, **refstack)
-    return observe_loss(criterion, observes, refobserves)
-
-  trainloss(loss_gen, optimizer,)
-
-if __name__ == "__main__":
-  main()
---
+++ tests/test_stack.py
@@ -0,0 +1,84 @@
+from asl.type import Type, Function
+from asl.callbacks import tb_loss, every_n
+from asl.util import draw, trainloader, as_img, iterget
+from asl.train import trainloss, log, log_append
+from asl.structs.nstack import neural_stack, ref_stack
+
+import torch
+from torch.autograd import Variable
+from torch import optim, nn
+
+def stack_trace(items, push, pop, empty):
+    """Example stack trace"""
+    log_append("empty", empty)
+
+    observes = []
+    stack = empty
+    # print("mean", items[0].mean())
+    (stack,) = push(stack, items[0])
+    (stack,) = push(stack, items[1])
+    # (stack,) = push(stack, items[2])
+    (pop_stack, pop_item) = pop(stack)
+    observes.append(pop_item)
+    (pop_stack, pop_item) = pop(pop_stack)
+    observes.append(pop_item)
+    log_append("observes", observes)
+    return observes
+
+
+def plot_observes(i, log, writer, **kwargs):
+  "Show the empty set in tensorboardX"
+  refobserves = log['observes'][0]
+  nobserves = log['observes'][1]
+  for j in range(len(refobserves)):
+    writer.add_image('compare{}/ref'.format(j), refobserves[j][0], i)
+    writer.add_image('compare{}/neural'.format(j), nobserves[j][0], i)
+
+
+def plot_empty(i, log, writer, **kwargs):
+  "Show the empty set in tensorboardX"
+  img = log['empty'][0][0]
+  writer.add_image('EmptySet', img, i)
+
+def observe_loss(criterion, obs, refobs, state=None):
+  "MSE between observations from reference and training stack"
+  total_loss = 0.0
+  losses = [criterion(obs[i], refobs[i]) for i in range(len(obs))]
+  print([loss[0].data[0] for loss in losses])
+  total_loss = sum(losses)
+  return total_loss
+
+
+def test_stack():
+  batch_size = 128
+  tl = trainloader(batch_size)
+  dataiter = iter(tl)
+  refiter = iter(tl)
+  matrix_stack = Type("Stack", (1, 28, 28), dtype="float32")
+  mnist_type = Type("mnist_type", (1, 28, 28), dtype="float32")
+  nstack = neural_stack(mnist_type, matrix_stack)
+  refstack = ref_stack(mnist_type, matrix_stack)
+
+  criterion = nn.MSELoss()
+  optimizer = optim.Adam(nstack.parameters(), lr=0.0001)
+
+  def loss_gen():
+    try:
+      items = iterget(dataiter, 3)
+      ref_items = iterget(refiter, 3)
+    except StopIteration:
+      print("End of Epoch")
+      items_iter = iter(tl)
+      ref_items_iter = iter(tl)
+      items = iterget(items_iter, 3)
+      ref_items = iterget(ref_items_iter, 3)
+
+
+    observes = stack_trace(items, **nstack)
+    refobserves = stack_trace(ref_items, **refstack)
+    return observe_loss(criterion, observes, refobserves)
+
+  trainloss(loss_gen, optimizer,)
+
+if __name__ == "__main__":
+  test_stack()
