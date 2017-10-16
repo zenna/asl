3b940f4f29952451ec83eb7e2f43f2c45bfe1a21
Zenna Tavares <zennatavares@gmail.com>
2017-10-16 01:05:59 -0400

    run

--- asl/hyper/run.sh
+++ asl/hyper/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-#SBATCH -J scalar_field
+#SBATCH -J itchangedyeah
 #SBATCH --gres=gpu:titan-x:1
 source activate tf
-srun -n1 --mem=32000 --gres=gpu:titan-x:1 python "$@"
+srun -n1 --gres=gpu:titan-x:1 --mem=64G python "$@"
