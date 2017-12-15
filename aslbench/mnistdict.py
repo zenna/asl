7bface248c0fc81f86e40ca66beb5588829922de
Zenna Tavares <zennatavares@gmail.com>
2017-12-15 11:42:10 -0500

    fix mnistdict

--- aslbench/mnistdict.py
+++ aslbench/mnistdict.py
@@ -1,38 +1,25 @@
-from aslbench.types import vec_stack, bern_seq, matrix_stack
-import aslbench.common as common
-import asl.opt
-
-
-import torch
+"Stack learned from reference"
 from typing import List
 import asl
-import asl.opt
-from asl.structs.ndict import SetItemNet, GetItemNet
 from asl.modules.modules import ConstantNet, ModuleDict
-from asl.util.misc import cuda
-from asl.type import Type
-from asl.sketch import Sketch
-from asl.callbacks import print_loss, converged, save_checkpoint, load_checkpoint, validate, every_n
-from asl.util.data import mnistloader
-from asl.log import log_append
-from asl.train import train
 from asl.structs.ndict import ref_dict
-from torch import optim
+from torch import optim, nn
 import common
+from multipledispatch import dispatch
 
-class DictSketch(Sketch):
+class DictSketch(asl.Sketch):
   def sketch(self, items, set_item, get_item, empty):
     """Example dict trace"""
-    log_append("empty", empty)
+    asl.log_append("empty", empty)
     adict = empty
     k1 = next(items)
     k2 = next(items)
     (adict,) = set_item(adict, k1, next(items))
-    log_append("{}/internal".format(self.mode.name), adict)
+    asl.log_append("{}/internal".format(self.mode.name), adict)
     (v, ) = get_item(adict, k1)
     self.observe(v)
     (adict,) = set_item(adict, k2, next(items))
-    log_append("{}/internal".format(self.mode.name), adict)
+    asl.log_append("{}/internal".format(self.mode.name), adict)
     (v2, ) = get_item(adict, k2)
     self.observe(v2)
     (v, ) = get_item(adict, k1)
@@ -44,49 +31,61 @@ def mnist_args(parser):
   parser.add_argument('--nitems', type=int, default=3, metavar='NI',
                       help='number of iteems in trace (default: 3)')
 
+mnist_size = (1, 28, 28)
+
+class MatrixDict(asl.Type):
+  typesize = mnist_size
+
+class Mnist(asl.Type):
+  typesize = mnist_size
+
+@dispatch(Mnist, Mnist)
+def dist(x, y):
+  return nn.MSELoss()(x.value, y.value)
+
 def train_dict():
+  # Get options from command line
   opt = asl.opt.handle_args(mnist_args)
   opt = asl.opt.handle_hyper(opt, __file__)
-  nitems = 3
-  mnist_size = (1, 28, 28)
 
-  class MatrixDict(Type):
-    size = mnist_size
+  class GetItem(asl.Function, asl.Net):
+    def __init__(self="GetItem", name="GetItem", **kwargs):
+      asl.Function.__init__(self, [MatrixDict, Mnist], [Mnist])
+      asl.Net.__init__(self, name, **kwargs)
 
-  class Mnist(Type):
-    size = mnist_size
+  class SetItem(asl.Function, asl.Net):
+    def __init__(self="SetItem", name="SetItem", **kwargs):
+      asl.Function.__init__(self, [MatrixDict, Mnist, Mnist], [MatrixDict])
+      asl.Net.__init__(self, name, **kwargs)
 
-  tl = mnistloader(opt.batch_size)
-  ndict = ModuleDict({'set_item': SetItemNet(MatrixDict, Mnist, Mnist, arch=opt.arch, arch_opt=opt.arch_opt),
-                       'get_item': GetItemNet(MatrixDict, Mnist, Mnist, arch=opt.arch, arch_opt=opt.arch_opt),
+  ndict = ModuleDict({'get_item': GetItem(arch=opt.arch,
+                                    arch_opt=opt.arch_opt),
+                       'set_item': SetItem(arch=opt.arch,
+                                  arch_opt=opt.arch_opt),
                        'empty': ConstantNet(MatrixDict)})
 
   dict_sketch = DictSketch([List[Mnist]], [Mnist], ndict, ref_dict())
-  cuda(dict_sketch)
-  loss_gen = asl.sketch.loss_gen_gen(dict_sketch, tl, asl.util.data.train_data)
-  optimizer = optim.Adam(ndict.parameters(), lr=opt.lr)
+  asl.cuda(dict_sketch)
 
+  # Loss
+  mnistiter = asl.util.mnistloader(opt.batch_size)
+  loss_gen = asl.sketch.loss_gen_gen(dict_sketch,
+                                     mnistiter,
+                                     lambda x: Mnist(asl.util.data.train_data(x)))
+
+  # Optimization details
+  optimizer = optim.Adam(ndict.parameters(), lr=opt.lr)
   asl.opt.save_opt(opt)
   if opt.resume_path is not None and opt.resume_path != '':
-    load_checkpoint(opt.resume_path, ndict, optimizer)
+    asl.load_checkpoint(opt.resume_path, ndict, optimizer)
 
-  tl_test = mnistloader(opt.batch_size, False)
-  # FIXME Rename Traindata
-  test_loss_gen = asl.sketch.loss_gen_gen(dict_sketch, tl_test, asl.util.data.train_data)
-  validate_cb = every_n(validate(test_loss_gen), 1000)
-
-  train(loss_gen,
-        optimizer,
-        cont=converged(1000),
-        callbacks=[print_loss(100),
+  asl.train(loss_gen, optimizer, maxiters=100000,
+        cont=asl.converged(1000),
+        callbacks=[asl.print_loss(100),
                    common.plot_empty,
                    common.plot_observes,
-                   common.plot_internals,
-                   save_checkpoint(1000, ndict),
-                   validate_cb],
+                   asl.save_checkpoint(1000, ndict)],
         log_dir=opt.log_dir)
-  print('Finished Training')
-
 
 
 if __name__ == "__main__":
