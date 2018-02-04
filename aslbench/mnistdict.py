e303596f8173ad30b310032ef2bf24c7669fc435
Zenna Tavares <zennatavares@gmail.com>
2018-02-04 00:19:36 -0500

    make harder

--- aslbench/mnistdict.py
+++ aslbench/mnistdict.py
@@ -22,17 +22,27 @@ def dicttracegen(nitems, nrounds):
     # keyset = [next(items) for i in range(nitems)]
     k1 = next(items)
     k2 = next(items)
-    (adict,) = set_item(adict, k1, next(items))
+    v1 = next(items)
+    v2 = next(items)
+    asl.log_append("{}/internal".format(runstate['mode']), v1)
+    asl.log_append("{}/internal".format(runstate['mode']), v2)
+
+    (adict,) = set_item(adict, k1, v1)
     asl.log_append("{}/internal".format(runstate['mode']), adict)
-    (v, ) = get_item(adict, k1)
-    asl.observe(v, "val1", runstate)
-    (adict,) = set_item(adict, k2, next(items))
+
+    (adict,) = set_item(adict, k2, v2)
     asl.log_append("{}/internal".format(runstate['mode']), adict)
-    (v2, ) = get_item(adict, k2)
-    asl.observe(v2, "val2a", runstate)
-    (v, ) = get_item(adict, k1)
-    asl.observe(v2, "val2a", runstate)
-    return v
+    (v1x, ) = get_item(adict, k1)
+    (v2x, ) = get_item(adict, k2)
+    asl.observe(v1x, "val1", runstate)
+    asl.observe(v2x, "val2", runstate)
+    (adict,) = set_item(adict, k2, v1)
+    asl.log_append("{}/internal".format(runstate['mode']), adict)
+    (v1xx, ) = get_item(adict, k1)
+    (v2xx, ) = get_item(adict, k2)
+    asl.observe(v1xx, "val1_2", runstate)
+    asl.observe(v2xx, "val2_2", runstate)
+    return v2xx
 
   return dicttrace
 
