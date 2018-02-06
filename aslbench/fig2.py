3ce27a058298fdf1f842fdf0c779cc9cd33713b1
Zenna Tavares <zennatavares@gmail.com>
2018-02-06 02:27:39 -0500

    fig

--- aslbench/fig2.py
+++ aslbench/fig2.py
@@ -44,8 +44,6 @@ def walkload(searchdir, isgood, loaddata):
   return data
 
 ## Make figure 1
-
-
 def nm_to_df(dfdata):
   nm_to_df_ = {}
   for df in dfdata:
@@ -55,7 +53,6 @@ def nm_to_df(dfdata):
     except:
       print("Missing data!")
   return nm_to_df_
-
 
 def nm_to_opt(optdata):
   nm_to_opt_ = {}
@@ -67,14 +64,14 @@ def nm_to_opt(optdata):
       print("Missing data!")
   return nm_to_opt_
 
-def optrunnames(optdata):
-  optrunnames = []
-  for df in dfdata:
-    try:
-      optrunnames.append(df['runname'][0:1][0])
-    except:
-      print("Missing data!")
-  return optrunnames  
+# def optrunnames(optdata, dfdata):
+#   optrunnames = []
+#   for df in dfdata:
+#     try:
+#       optrunnames.append(df['runname'][0:1][0])
+#     except:
+#       print("Missing data!")
+#   return optrunnames  
 
 # Get all the data frames where the number of items is 1, find the optimal loss
 
@@ -85,7 +82,7 @@ def dfs_where_opt(filter_func, nm_to_df_, nm_to_opt_):
       dfs.append(nm_to_df_[nm])
   return dfs
 
-def data(path = "/home/zenna/sshfs/omdata/runs/mnistsetsun"):
+def data(path):
   optdata = walkoptdata(path)
   dfdata = walkdfdata(path)
   nm_to_df_ = nm_to_df(dfdata)
@@ -133,4 +130,22 @@ def loss_curve_for_item(nitems, nm_to_df_, nm_to_opt_):
 #   get_df_where_opt(nitemsisnitems)
 
 # Figure 1.b) train capacity test
-res = record_data("/home/zenna/sshfs/omdata/runs/mnistsetsun/bsev_Feb04_18-37-19_node038_presuperbowl")
+def optim_opt_df(nm_to_df_, nm_to_opt_, nitems):
+  dfs = dfs_where_opt(lambda opt: opt['nitems'] == nitems, nm_to_df_, nm_to_opt_)
+  min_losses = [min(df['loss']) for df in dfs]
+  optim_id = min_losses.index(min(min_losses))
+  optim_df = dfs[optim_id]
+  optim_opt = nm_to_opt_[runname(optim_df)]
+  return optim_opt, optim_df
+
+def matrix_plot(nm_to_df_, nm_to_opt_):
+  nitems = 1
+  optim_opt, optim_df = optim_opt_df(nm_to_df_, nm_to_opt_, nitems)
+  res = record_data(optim_opt["log_dir"], {"nitems":1})
+  return res
+  # res = record_data("/home/zenna/sshfs/omdata/runs/mnistsetsun/bsev_Feb04_18-37-19_node038_presuperbowl")
+
+if __name__ == "__main__":
+  path = "/home/zenna/sshfs/omdata/runs/mnistsetsun"
+  nm_to_df_, nm_to_opt_ = data(path)
+  res = matrix_plot(nm_to_df_, nm_to_opt_)
